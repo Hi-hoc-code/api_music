a14d46451eecdc1409bee46581c54796f4f6b950
HiHocCode <ngoctyho1612@gmail.com>
2024-10-30 17:44:52 +0700

    reset

--- src/routes/adminRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const AdminController = require('../controllers/AdminController');
-const authMiddleware = require('../middlewares/auth');
-
-router.post('/login', AdminController.login);
-router.get('/users', authMiddleware, AdminController.getAllUsers);
-
-module.exports = router;
--- src/routes/songRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const SongController = require('../controllers/SongController');
-
-router.get('/', SongController.getAllSongs);
-router.get('/:id', SongController.getSongById);
-
-module.exports = router;
